4648c2746ba40eca9e719703a0d24f22219eab42
Vicram Rajagopalan <vicramrajagopalan@gmail.com>
2019-10-02 22:22:50 -0500

    Implemented first method for doing PCA

--- helpers.py
+++ helpers.py
@@ -14,6 +14,8 @@ def standardize(X):
     """
     Given a data matrix X with samples on the rows and features on the columns,
     center each column and scale each column to unit variance.
+
+    If any of the columns has a standard deviation of 0, a ValueError will be raised.
     """
     checkmat(X)
 
@@ -26,7 +28,8 @@ def standardize(X):
     # Here, I use ddof=0 for no reason other than that sklearn chooses to use ddof=0. See docs:
     # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html#sklearn.preprocessing.StandardScaler
     assert std_devs.shape == (1, n_features)
-
+    if np.isin(0.0, std_devs):
+        raise ValueError("Would divide by zero due to a standard deviation of zero")
     X_standardized = (X - means) / std_devs
     assert X_standardized.shape == X.shape
 
--- test_pca.py
+++ test_pca.py
@@ -6,17 +6,83 @@ Run the tests using pytest.
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+import pytest
 
-from helpers import standardize
+from helpers import (standardize, checkmat)
 
-def test_main(X):
-    assert X.ndim == 2
-    assert X.size > 0
+Xlist = [
+    np.array([[1, 2, 3],
+              [2, 3, 4]]),
+    np.array([[1, 2],
+              [2, 1]])
+]
+
+@pytest.mark.parametrize(
+    "X,newdim", 
+    list(zip(Xlist, [1, 1]))
+)
+def test_main(X, newdim):
+    checkmat(X)
 
     n_samples = X.shape[0]
     n_features = X.shape[1]
+    assert newdim <= n_features
 
-    X = standardize(X)
+    X_scaled = standardize(X)
 
+    # Method 1 of doing PCA: compute eigenpairs of the covariance matrix (X^T times X)
+    XTX = X_scaled.T @ X_scaled
+    assert XTX.shape == (n_features, n_features)
+    (eigvals, eigvecs) = np.linalg.eig(XTX)
+    assert eigvals.shape == (n_features,)
+    assert eigvecs.shape == (n_features, n_features) # the eigvecs are on the columns
+    # The learned model consists of the eigvecs corresponding to the newdim-highest eigvals. 
+    # This requires sorting by the eigenvals.
+    indices = np.argsort(eigvals)
+    assert indices.shape == (n_features,)
+    top_indices = indices[-newdim:]
+    assert top_indices.shape == (newdim,)
+    top_eigvecs = eigvecs[:, top_indices]
+    assert top_eigvecs.shape == (n_features, newdim)
+
+
+
+@pytest.mark.parametrize("X", Xlist)
 def test_standardize(X):
+    checkmat(X)
+
+    X_mine = standardize(X)
+
     scaler = StandardScaler()
+    X_sklearn = scaler.fit_transform(X)
+    
+    assert np.allclose(X_mine, X_sklearn)
+
+def test_sort():
+    """
+    Tests that argsort + numpy advanced indexing work like I think they do.
+    This just makes sure that I'm getting the right eigenvectors.
+    """
+    eigvals = np.array([1, 4, 2, 0, 3])
+    eigvecs = np.array([[1, 2, 3, 4, 5],
+                        [6, 7, 8, 9, 10]])
+    assert eigvecs.shape == (2, 5)
+
+    indices = np.argsort(eigvals)
+    assert np.array_equal(
+        indices,
+        np.array([3, 0, 2, 4, 1])
+    )
+    assert np.array_equal(
+        eigvals[indices],
+        np.array([0, 1, 2, 3, 4])
+    )
+
+    top3 = indices[-3:]
+    top_eigvecs = eigvecs[:, top3]
+    assert top_eigvecs.shape == (2, 3)
+    assert np.array_equal(
+        top_eigvecs,
+        np.array([[3, 5, 2],
+                  [8, 10, 7]])
+    )
